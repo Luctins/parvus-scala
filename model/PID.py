320c6184fad3860e5fe470ee277852f5531464c6
luctins <lucas.martinsmendes@gmail.com>
2021-03-03 15:13:27 -0300

    Adiciona lógica para passar o delta_t para o controlador, documenta código em geral,
    muda pasta logfiles para 'log/'

--- model/PID.py
+++ model/PID.py
@@ -13,6 +13,7 @@ signal - with PID controller
 
 from pymodbus.client.sync import ModbusTcpClient
 import sys
+import os
 import time
 from functools import reduce
 from simple_pid import PID
@@ -127,47 +128,51 @@ def run_sim(client, contr, logf, setpoint, out_valve, in_valve, \
 		return [n] + l[:-1]
 
 	#Constants
-	STOP_TIMEOUT = 100
+	STOP_TIMEOUT = 1000
+	#Voltage offset between modbus data ranges (int) 0-1000 and (float) 0.0-1.0
 	V_OFS = DEC_OFS*TANK_MAX_LVL
-	T_step = _T_step/T_scale
+	T_step = _T_step/T_scale #timestep adjusted for the timescale
 
 	print("""
 	Simulation parameters
 	ctrl: {}
 	setpoint: {}
 	T_step: {}
-	""".format(contr.auto_mode, setpoint, T_step))
+	T_scale: {}
+	""".format(contr.auto_mode, setpoint, T_step, T_scale))
 
 	pause_sim(client);
 
+	#Print logfile header if not continuing simulation
+	#Out valve is the control signal if controller connected
 	if not _continue_sim:
-		line = 't,\tlevel,\toutflow, \tout_valve, \tsetpoint,,\tin_valve'
+		line = 't,level,outflow,out_valve,setpoint,in_valve'
 		print(line)
 		log.write(line.encode('utf-8')+b'\n')
 
 	last_l = [ -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-	stop_time = 0
-	start_time = time.time()
 	level = 0
+	ret = in_valve #return value
+	stop_time = 0
 
 	if _no_stop:
 		print('running in continuos mode, press "q RET" to stop')
 
+	#write initial values
 	write_in_valve(int(in_valve*V_OFS), client)
 	write_out_valve(int(out_valve*V_OFS), client)
 
+	#start simulation and unpause
 	start_sim(client)
 	unpause_sim(client)
 
-
-	#TODO: measure deltat between updates (modbus commands)
-
-	#check if controller enabled, if not run until tank reaches 50% point
+	start_time = time.time()
+	#check if controller enabled, if not run until tank level stabilizes
 	if not contr.auto_mode:
-
 		while True:
 			level, outflow = read_in_reg(client).registers
 
+			#log data to file
 			line = w_log(time.time() - start_time, level/V_OFS,\
 						 outflow/V_OFS, out_valve, setpoint, in_valve,\
 						 logf=log)
@@ -188,52 +193,59 @@ def run_sim(client, contr, logf, setpoint, out_valve, in_valve, \
 	else:
 		contr.setpoint = setpoint;
 		client.write_register(3, int(setpoint), unit=CLP_UNIT)
-		contr.set_auto_mode(True, out_valve)
+		contr.set_auto_mode(True, out_valve) #pass to controller last level value
+		last_t = time.time()
 		while True:
 			iter_t = time.time() #measure iteration time
 
 			#get modbus registers
 			level, outflow, setpoint = read_in_reg(client).registers
+			level /= V_OFS #scale down values from 0-1000 -> 0.0-1.0
 
 			if _read_sp:
 				if setpoint:
 					contr.setpoint = setpoint/(V_OFS)
 					client.write_register(3, int(setpoint), unit=CLP_UNIT)
 
-			level /= V_OFS #scale down values from 0-1000 -> 0.0-1.0
-			c = contr(level)
-			write_out_valve(int(c*V_OFS), client) #write to system
+			#Obtain control signal, also adjust delta time for Timescale
+			c = contr(level, dt=(time.time() - last_t)*T_scale)
+			last_t = time.time()
 
+			write_out_valve(int(c*V_OFS), client) #write control signal to sys
+
+			#write log data to file
 			line = w_log(time.time() - start_time, level, outflow/V_OFS, c,\
 						 contr.setpoint, in_valve, logf=log)
-
 			#stop cond
 			if (not _no_stop):
 				last_l = ap_l(last_l, int(level*DEC_OFS))
 				if stop_time:
 					if (time.time() - stop_time) > (STOP_TIMEOUT/T_scale):
 						print("timeout reached, done")
+						ret = c;
 						break
 				elif all_is_same(last_l):
 					stop_time = time.time()
 					print("started counting timeout")
 
-			#read char from console
+			#Read char from console
 			v = select.select([sys.stdin], [], [], 0)[0]
 			if v:
 				k = v[0].read(1)
-				if k[0] == 'q':
-					break
 				sys.stdin.flush()
+				if k[0] == 'q':
+					ret = c;
+					break
 
-			d = time.time() - iter_t
 			print(line, "dt: {}".format(int(d*1000)), "\t", last_l)
+			d = time.time() - iter_t
 			time.sleep(T_step - d if d < T_step else 0.0) # delay at most T_step
 
 	if _stop_sim:
-		stop_sim(client);
+		stop_sim(client)
 	else:
 		pause_sim(client)
+	return ret #return the last control value, to be passed as a arg to the controller
 
 
 #-------------------------------------------------------------------------------
@@ -256,6 +268,15 @@ else:
 		print('provided ip', args.ip, ' is invalid')
 		sys.exit(-1)
 
+# Open logfile
+logdir = 'log'
+logname = 'data_log{}.csv'.format(int(time.time()))
+if not os.path.exists('./'+logdir+'/'):
+	os.mkdir(logdir)
+	print('created logdir:', logdir)
+log = open((logdir+'/'+logname).encode('utf8'), 'wb')
+print('opened logfile:', logname)
+sys.exit(0)
 #------------------------------
 # PID Controller
 pid = PID()
@@ -280,6 +301,8 @@ time.sleep(1)
 #-----------------------------------------------------------
 # Run simulation
 
+
+
 # Reset registers
 rq = client.write_coils(0, [False]*3, unit=CLP_UNIT)
 assert(rq.function_code < 0x80)
@@ -288,20 +311,15 @@ print('reset coils')
 stop_sim(client) #clean previous state
 
 #Create Logfile
-logname = 'data_log{}.csv'.format(int(time.time()))
-log = open(logname.encode('utf8'), 'wb')
-print('opened logfile:', logname)
+
 
 # (setup) Run with no controller until level stabilizes at 5
+pid.auto_mode = 0
+last_v = run_sim(client, pid, log, 0.0, 0.5, 0.5, T_scale=4, _T_step=0.01)
 
-#pid.auto_mode = 0
-#run_sim(client, pid, log, 0.0, 5.0, 5.0, T_scale=1)
 #Step output valve and connect controller
-
-#time.sleep(10);
-
 pid.auto_mode = 1
-run_sim(client, pid, log, 0.5, 0.5, 0.6, _stop_sim=1, T_scale=1, _T_step=0.01, \
+run_sim(client, pid, log, 0.5, last_v, 0.6, _stop_sim=1, T_scale=4, _T_step=0.01, \
 		_no_stop=1, _read_sp=1)
 
 time.sleep(2)
